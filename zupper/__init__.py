40c69df54d98e14e273d775fdf30d5d43df8d1b5
Michael Hucka <mhucka@caltech.edu>
2020-11-15 08:51:07 -0800

    Add some basic files to start things off

---
+++ zupper/__init__.py
@@ -0,0 +1,39 @@
+'''
+Zupper: a tool to write Zotero item URIs into Zotero article PDF files
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by Michael Hucka and the California Institute of Technology.
+This code is open-source software released under the MIT license.  Please see
+the file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.1'
+__description__ = 'Write Zotero item URIs into article PDF files'
+__url__         = 'https://github.com/mhucka/zupper'
+__author__      = 'Michael Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'MIT License'
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
