1628cc83f0b7d5372b6121e2756c89399546773f
Michael Hucka <mhucka@caltech.edu>
2020-11-23 07:11:47 -0800

    WIP fleshing out the body

--- zupper/__main__.py
+++ zupper/__main__.py
@@ -1,5 +1,5 @@
 '''
-Zupper: a program to write Zotero item URIs into Zotero article PDF files
+Zuppa: a program to write Zotero item URIs into Zotero article PDF files
 
 Authors
 -------
@@ -14,10 +14,13 @@ This code is open-source software released under a 3-clause BSD license.
 Please see the file "LICENSE" for more information.
 '''
 
+from   commonpy.data_utils import timestamp
+from   commonpy.interrupt import config_interrupt
 from   boltons.debugutils import pdb_on_signal
 import os
 from   os import path, cpu_count
 import plac
+from   quiche import UI, inform, warn, alert, alert_fatal
 import signal
 import sys
 from   sys import exit as exit
@@ -25,51 +28,92 @@ from   sys import exit as exit
 if __debug__:
     from sidetrack import set_debug, log, logr
 
-import zupper
-from zupper import print_version
+import zuppa
+from zuppa import print_version
 from .exceptions import *
 from .exit_codes import ExitCode
-from .ui import inform, warn, alert, alert_fatal
+from .main_body import MainBody
 
 # .............................................................................
 
 @plac.annotations(
-    no_color   = ('do not color-code terminal output',                 'flag',   'C'),
-    no_comment = ('do not write URI into macOS Finder comment',        'flag',   'F'),
-    lastmod    = ('ignore file whose modification dates predate "L"',  'option', 'l'),
-    quiet      = ('only print important messages while working',       'flag',   'q'),
-    watch      = ('continuously watch for new files and update them',  'flag',   'w'),
-    version    = ('print version info and exit',                       'flag',   'V'),
-    debug      = ('write detailed trace to "OUT" ("-" means console)', 'option', '@'),
+    api_key    = ('use API key "A" to access the Zotero API service',        'option', 'a'),
+    after_date = ('only act on files created or modified after date "D"',    'option', 'd'),
+    identifier = ('Zotero library identifier',                               'option', 'i'),
+    methods    = ('select how the URIs are to be stored (default: link)',    'option', 'm'),
+    dry_run    = ('report what would be done without actually doing it',     'flag',   'n'),
+    quiet      = ('be less chatty -- only print important messages',         'flag',   'q'),
+    version    = ('print version info and exit',                             'flag',   'V'),
+    watch_mode = ('continuously watch for new files and update them',        'flag',   'w'),
+    no_keyring = ('do not store credentials in a keyring service',           'flag',   'X'),
+    no_color   = ('do not color-code terminal output',                       'flag',   'Y'),
+    debug      = ('write detailed trace to "OUT" ("-" means console)',       'option', '@'),
     files      = 'file(s) and/or folder(s) containing Zotero article PDF files',
 )
 
-def main(no_color = False, quiet = False, version = False, debug = 'OUT', files):
-    '''Zupper (a loose acronym of "Zotero URIs into PDF Properties").
+def main(api_key = 'A', after_date = 'D', identifier = 'I', methods = 'M',
+         dry_run = False, quiet = False, version = False, watch_mode = False,
+         no_keyring = False,  no_color = False, debug = 'OUT', *files):
+    '''Zuppa (a loose acronym of "Zotero URIs into PDF Properties").
 
-Zupper writes Zotero item URIs into the metadata of PDF files.  It also adds
+Zuppa writes Zotero item URIs into the metadata of PDF files.  It also adds
 the URIs to the files' macOS Spotlight file comments.
 
 Credentials for Zotero access
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+Zuppa needs a library identifier and API key in order to retrieve information
+about your Zotero entries.  By default, it tries to get this information from
+the system keychain.  If the information does not exist in the keychain from a
+previous run of Zuppa, it will ask the user interactively for the identifier
+and API key, and (unless the -K option is given) store them in the user's
+keychain so that it does not have to ask again in the future.  It is also
+possible to supply the information directly on the command line using the
+-i and -a options; the given values will override the values stored in the
+keychain.  (This is also how you can replace previously-stored values: use
+-i and/or -a, without -K, and the new values will override the stored values.)
+
+When using the -i option, the library identifier can be prefixed with the
+string "user:" or "group:" to indicate whether the identifier refers to a
+personal or group library.  For example,
+
+  zuppa -i user:1234578 ...
+
+If -i is used and the value does not begin with "user:" or "group:", then it
+is assumed to be a personal library identifier.
 
 Basic usage
 ~~~~~~~~~~~
+
+The most convenient way to run Zuppa is to let it store your credentials in
+your keychain so that you do not have to provide them each time or write them
+on the command line.
 
 
 Watch mode
 ~~~~~~~~~~
 
+If given the option -w, Zuppa will go into continuous watch mode: instead
+of exiting after processing the arguments (i.e., gathering a list of PDF files,
+filtering by last modification date, and updating file metadata), it will keep
+running and watching for changes in any of the arguments.  If any files are
+modified or added (including files within any folders listed on the command
+line), Zuppa repeats its normal processing for them.  For example:
+
+  zuppa -f -w ~/zotero/storage
+
+will start Zuppa in watch mode on a Zotero storage directory, with the
+--finder-too option to update both the PDF files and the Finder comments.
+
 
 Additional command-line arguments
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-If given the -q option, Zupper will not print its usual informational messages
+If given the -q option, Zuppa will not print its usual informational messages
 while it is working.  It will only print messages for warnings or errors.
-By default messages printed by Zupper are also color-coded.  If given the
-option -C, Zupper will not color the text of messages it prints.  (This latter
-option is useful when running Zupper within subshells inside other environments
+By default messages printed by Zuppa are also color-coded.  If given the
+option -C, Zuppa will not color the text of messages it prints.  (This latter
+option is useful when running Zuppa within subshells inside other environments
 such as Emacs.)
 
 If given the -V option, this program will print the version and other
@@ -79,8 +123,8 @@ If given the -@ argument, this program will output a detailed trace of what it
 is doing.  The debug trace will be sent to the given destination, which can
 be '-' to indicate console output, or a file path to send the output to a file.
 
-When -@ has been given, Zupper also installs a signal handler on signal SIGUSR1
-that will drop Zupper into the pdb debugger if the signal is sent to the
+When -@ has been given, Zuppa also installs a signal handler on signal SIGUSR1
+that will drop Zuppa into the pdb debugger if the signal is sent to the
 running process.
 
 
@@ -103,11 +147,7 @@ Command-line arguments summary
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 
-    # Initial setup -----------------------------------------------------------
-
-    ui = UI('Zupper', 'Zotero URIs into PDF Properties',
-            use_color = not no_color, be_quiet = quiet)
-    ui.start()
+    # Set up debug logging as soon as possible, if requested ------------------
 
     if debug != 'OUT':
         if __debug__: set_debug(True, debug)
@@ -119,6 +159,10 @@ Command-line arguments summary
 
     # Preprocess arguments and handle early exits -----------------------------
 
+    ui = UI('Zuppa', 'Zotero URI PDF Property Annotator',
+            use_color = not no_color, be_quiet = quiet)
+    ui.start()
+
     if version:
         print_version()
         exit(int(ExitCode.success))
@@ -128,7 +172,17 @@ Command-line arguments summary
     if __debug__: log('='*8 + f' started {timestamp()} ' + '='*8)
     body = exception = None
     try:
-        pass
+        body = MainBody(files       = files,
+                        api_key     = None if api_key == 'A' else api_key,
+                        library_id  = None if identifier == 'I' else identifier,
+                        after_date  = None if after_date == 'D' else after_date,
+                        use_keyring = not no_keyring,
+                        methods     = methods,
+                        watch_mode  = watch_mode,
+                        dry_run     = dry_run)
+        config_interrupt(body.stop, UserCancelled(ExitCode.user_interrupt))
+        body.run()
+        exception = body.exception
     except Exception as ex:
         exception = sys.exc_info()
 
@@ -136,10 +190,10 @@ Command-line arguments summary
 
     exit_code = ExitCode.success
     if exception:
+        if __debug__: log(f'main body returned an exception: {exception}')
         if exception[0] == CannotProceed:
             exit_code = exception[1].args[0]
         elif exception[0] in [KeyboardInterrupt, UserCancelled]:
-            if __debug__: log(f'received {exception.__class__.__name__}')
             warn('Interrupted.')
             exit_code = ExitCode.user_interrupt
         else:
@@ -156,10 +210,8 @@ Command-line arguments summary
     # And exit ----------------------------------------------------------------
 
     if __debug__: log('_'*8 + f' stopped {timestamp()} ' + '_'*8)
-    if exit_code == ExitCode.user_interrupt:
-        os._exit(int(exit_code))
-    else:
-        exit(int(exit_code))
+    if __debug__: log(f'exiting with exit code {exit_code}')
+    exit(int(exit_code))
 
 
 # Main entry point.
