987b983dea1c4497688892f35a472c031d279c9d
Michael Hucka <mhucka@caltech.edu>
2020-11-24 10:42:44 -0800

    Modified to work with new understanding of keyring functions

--- zuppa/keyring_utils.py
+++ zuppa/keyring_utils.py
@@ -14,6 +14,7 @@ This code is open-source software released under a 3-clause BSD license.
 Please see the file "LICENSE" for more information.
 '''
 
+from   bun import alert
 import getpass
 import keyring
 import sys
@@ -21,6 +22,12 @@ import sys
 if sys.platform.startswith('win'):
     import keyring.backends
     from keyring.backends.Windows import WinVaultKeyring
+if sys.platform.startswith('darwin'):
+    import keyring.backends
+    from keyring.backends.OS_X import Keyring
+
+if __debug__:
+    from sidetrack import log
 
 
 # Global constants.
@@ -33,38 +40,40 @@ _KEYRING = f'org.caltechlibrary.{__package__}'
 # Main functions.
 # .............................................................................
 
-def keyring_credentials(lib_type, lib_id):
-    '''Looks up the user's credentials.'''
-    if sys.platform.startswith('win'):
-        keyring.set_keyring(WinVaultKeyring())
-    if sys.platform.startswith('darwin'):
-        keyring.set_keyring(Keyring())
-    value = keyring.get_password(_KEYRING, lib_type + ':' + lib_id)
-    return _decoded(value) if value else (None, None, None)
-
-
-def save_keyring_credentials(api_key, lib_type, lib_id):
-    '''Saves the user's credentials.'''
-    if sys.platform.startswith('win'):
-        keyring.set_keyring(WinVaultKeyring())
-    if sys.platform.startswith('darwin'):
-        keyring.set_keyring(Keyring())
-    keyring.set_password(_KEYRING, 'zupper', _encoded(api_key, lib_type, lib_id))
-
-
-# Utility functions.
-# .............................................................................
-
 # Explanation about the weird way this is done: the Python keyring module
 # only offers a single function for setting a value; ostensibly, this is
 # intended to store a password associated with an identifier (a user name),
 # and this identifier is expected to be obtained some other way, such as by
 # using the current user's computer login name.  But, in our situation, we
-# have multiple pieces of information we have to store: the library id, the
-# library type, and the api key.  The hackacious solution taken here is to
-# concatenate several values into a single string used as the actual value
-# stored.  The individual values are separated by a character that is
-# unlikely to be part of any user-typed value.
+# have multiple pieces of information we have to store (a user id and an api
+# key).  The hackacious solution taken here is to concatenate the values into
+# a single string used as the actual value stored.  The individual values are
+# separated by a character that is unlikely to be part of any user-typed value.
+
+def keyring_credentials():
+    '''Looks up the user's credentials.'''
+    if sys.platform.startswith('win'):
+        keyring.set_keyring(WinVaultKeyring())
+    if sys.platform.startswith('darwin'):
+        keyring.set_keyring(Keyring())
+    value = keyring.get_password(_KEYRING, getpass.getuser())
+    if __debug__: log(f'got "{value}" from keyring {_KEYRING}')
+    return _decoded(value) if value else (None, None)
+
+
+def save_keyring_credentials(api_key, user_id):
+    '''Saves the user's credentials.'''
+    if sys.platform.startswith('win'):
+        keyring.set_keyring(WinVaultKeyring())
+    if sys.platform.startswith('darwin'):
+        keyring.set_keyring(Keyring())
+    value = _encoded(api_key, user_id)
+    if __debug__: log(f'storing "{value}" to keyring {_KEYRING}')
+    keyring.set_password(_KEYRING, getpass.getuser(), value)
+
+
+# Utility functions.
+# .............................................................................
 
 _sep = ''
 '''Character used to separate multiple actual values stored as a single
@@ -73,8 +82,8 @@ very unlikely to be part of a legitimate string value typed by user at a
 shell prompt, because control-c is normally used to interrupt programs.
 '''
 
-def _encoded(api_key, lib_type, lib_id):
-    return f'{api_key}{_sep}{lib_type}{_sep}{lib_id}'
+def _encoded(api_key, user_id):
+    return f'{api_key}{_sep}{user_id}'
 
 
 def _decoded(value_string):
