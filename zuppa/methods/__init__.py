db49e7e5af7161ecbf41e2c81480535ee7d2163b
Michael Hucka <mhucka@caltech.edu>
2020-11-26 07:51:26 -0800

    Split out writers into a separate set of classes

---
+++ zuppa/methods/__init__.py
@@ -0,0 +1,32 @@
+'''
+Zuppa module for implementing different methods of writing Zotero links
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by Michael Hucka and the California Institute of Technology.
+This code is open-source software released under a 3-clause BSD license.
+Please see the file "LICENSE" for more information.
+'''
+
+if __debug__:
+    from sidetrack import log
+
+from .findercomment import FinderComment
+from .pdfsubject import PDFSubject
+
+KNOWN_METHODS = {
+    'findercomment': FinderComment,
+    'pdfsubject': PDFSubject,
+}
+
+# Save this list to avoid recreating it all the time.
+METHODS_LIST = sorted(KNOWN_METHODS.keys())
+
+def methods_list():
+    return METHODS_LIST
---
+++ zuppa/methods/base.py
@@ -0,0 +1,87 @@
+'''
+base.py: base class definition for annotation methods in zuppa
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by Michael Hucka and the California Institute of Technology.
+This code is open-source software released under a 3-clause BSD license.
+Please see the file "LICENSE" for more information.
+'''
+
+from abc import ABC, abstractmethod
+
+if __debug__:
+    from sidetrack import log
+
+
+# Class definitions.
+# .............................................................................
+# Basics for the __eq__ etc. methods came from
+# https://stackoverflow.com/questions/1061283/lt-instead-of-cmp
+
+class WriterMethod(ABC):
+    def __init__(self):
+        pass
+
+
+    def __str__(self):
+        return self.name()
+
+
+    def __repr__(self):
+        return self.name()
+
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        else:
+            return not self.name() < other.name() and not other.name() < self.name()
+
+
+    def __ne__(self, other):
+        return not __eq__(self, other)
+
+
+    def __lt__(self, other):
+        return self.name() < other.name()
+
+
+    def __gt__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        else:
+            return other.name() < self.name()
+
+
+    def __le__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        else:
+            return not other.name() < self.name()
+
+
+    def __ge__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        else:
+            return not self.name() < other.name()
+
+
+    @property
+    @abstractmethod
+    def name(self):
+        '''Returns the canonical internal name for this method.'''
+        pass
+
+
+    @abstractmethod
+    def write_uri(self, file, link):
+        '''Write the link into the file.'''
+        pass
