37963d94545ea659c6844534026fbbadd922ae55
Michael Hucka <mhucka@caltech.edu>
2020-11-26 19:46:14 -0800

    First working version of pdfsubject method

--- zuppa/methods/pdfsubject.py
+++ zuppa/methods/pdfsubject.py
@@ -14,10 +14,13 @@ This code is open-source software released under a 3-clause BSD license.
 Please see the file "LICENSE" for more information.
 '''
 
-from .base import WriterMethod
+from   bun import inform
+from   pdfrw import PdfReader, PdfWriter
 
 if __debug__:
     from sidetrack import log
+
+from .base import WriterMethod
 
 
 # Class definitions.
@@ -28,5 +31,13 @@ class PDFSubject(WriterMethod):
         return 'pdfsubject'
 
 
-    def write_uri(self, file, uri):
-        pass
+    def write_uri(self, file, uri, dry_run):
+        '''Write the "uri" into the Subject attribute of PDF file "file".
+        The previous value will be overwritten.
+        '''
+        if __debug__: log(f'reading PDF file {file}')
+        trailer = PdfReader(file)
+        trailer.Info.Subject = uri
+        if not dry_run:
+            if __debug__: log(f'writing PDF file with new subject field: {file}')
+            PdfWriter(file, trailer = trailer).write()
