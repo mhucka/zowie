2f14f4db2b764df4759b645656c112ad772413d4
Michael Hucka <mhucka@caltech.edu>
2020-11-27 09:36:28 -0800

    More work on user help documentation

--- zuppa/__main__.py
+++ zuppa/__main__.py
@@ -57,96 +57,107 @@ def main(api_key = 'A', no_color = False, after_date = 'D', identifier = 'I',
          quiet = False, version = False, debug = 'OUT', *files):
     '''Zuppa ("Zotero URI PDF Property Annotator") is a tool for Zotero users.
 
-Zuppa writes Zotero item URIs into the PDF files and/or the macOS Spotlight
-comments of PDF files in the user's Zotero database.  This makes it possible
+Zuppa writes Zotero item URIs into the PDF files and/or the macOS Finder
+comments of PDF files in the user's Zotero database. This makes it possible
 to look up the Zotero entry of a PDF file from outside of Zotero.
 
 Credentials for Zotero access
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Zuppa needs a Zotero API key and the user's personal library identifier (also
-known as the "user ID for use in API calls").  By default, it tries to get
-this information from the system keychain.  If the information does not exist
-in the keychain from a previous run of Zuppa, it will ask the user
-interactively for the identifier and API key, and (unless the -K option is
-given) store them in the user's keychain so that it does not have to ask
-again in the future.  It is also possible to supply the information directly
-on the command line using the -i and -a options; the given values will then
-override the values stored in the keychain (unless the -K option is also
-given).  This is also how you can replace previously-stored values: use -a
-and -i (without -K) and the new values will override the stored values.
+Zuppa needs to know the user's personal library identifier (also known as the
+"userID") and a Zotero API key. By default, it tries to get this information
+from the user's keychain. If the values do not exist in the keychain from a
+previous run, Zuppa will ask the user, and (unless the -K option is given)
+store the values in the user's keychain so that it does not have to ask again
+in the future. It is also possible to supply the identifier and API key on
+the command line using the -i and -a options, respectively; the given values
+will then override the values stored in the keychain (unless the -K option is
+also given). This is also how you can replace previously-stored values: use
+-i and -a (without -K) and the new values will override the stored values.
 
-Zuppa uses the Zotero API to discover the user's shared libraries and groups.
-This allows it to look up Zotero URIs for PDFs regardless of whether they
-belong to the user's personal library or shared libraries.
+To find out your Zotero userID and create an API key, log in to your Zotero
+account at Zotero.org and visit https://www.zotero.org/settings/keys
 
 Basic usage
 ~~~~~~~~~~~
 
 Zuppa can operate on a folder, or one or more individual PDF files, or a mix
-of both.  Its basic mode of operation is to obtain the Zotero item URI of the
-bibliographic record containing each PDF file found, then write a Zotero
-select link into (depending on your choice) the macOS Finder comments of the
-PDF file, the PDF "Subject" metadata attribute, and/or the PDF "Producer"
-metadata attribute.  A Zotero select link has the form `zotero://select/...`
-and when opened on macOS, causes the Zotero desktop application to open that
-item in your database.
+of both. Suppose your local Zotero database is located in ~/my-zotero/. Perhaps
+the simplest way to run Zuppa is the following command:
 
-Suppose your local Zotero database is located in ~/my-zotero and Zotero's
-"storage" subfolder (for PDF files) is ~/my-zotero/storage/.  Perhaps the
-simplest way to run Zuppa is the following command:
+  zuppa ~/my-zotero
 
-  zuppa ~/my-zotero/storage
+If this is your first run of Zuppa, it will ask you for your userID and API
+key, then search for PDF files recursively under ~/my-zotero/.  For each PDF
+file found, Zuppa will contact the Zotero servers over the network and
+determine the item URI for the bibliographic entry containing that PDF
+file. Finally, it will use the default method of writing the Zotero link,
+which is to write it into the Finder comments for the PDF file.
 
-It will ask you for your Zotero API credentials (if this is your first run of
-Zuppa), then search for PDF files recursively under ~/my-zotero/storage/.
-For each PDF file found, Zuppa will contact the Zotero API server over the
-network and determine the item URI for the bibliographic entry containing that
-PDF file.  Finally, it will use the default method of writing the Zotero select
-link, which is to write it into the Finder comments for the file.
+Instead of a folder, you can invoke zuppa on one or more individual files (but
+be careful to quote pathnames with spaces in them, such as in this example):
+
+  zuppa "~/my-zotero/storage/26GS7CZL/Smith 2020 Paper.pdf"
+
+Zuppa supports multiple methods of writing the Zotero select link. The default
+is to write it into the Finder comments. The option -l will cause Zuppa to
+print a list of all the methods available, then exit. The option -m can be
+used to select one or more methods when running Zuppa. (Separate the method
+names with commas, without spaces.) For example,
+
+ zuppa -m findercomment,pdfsubject ~/my-zotero/storage
+
+will make Zuppa write the Zotero select link into the Finder comments as well
+as the PDF metadata attribute "Subject".
+
+At this time, the following methods are available:
+
+  findercomment: prepends the Zotero item URI to the Finder comments for
+    the file. It tries to be careful: if it finds a Zotero URI as the first
+    thing in the comments, it replaces that URI instead of prepending a new
+    one. However, Finder comments are notorious for being easy to damage or
+    lose, so beware that Zuppa may irretrievably corrupt any existing Finder
+    comments on the file.
+
+  pdfsubject: rewrites the Subject metadata field in the PDF file. This is
+    not the same as the Title field; for some users, the Subject field is not
+    used for any other purpose and thus can be usefully hijacked for the
+    purpose of storing the Zotero item URI. This makes the value accessible
+    from macOS Preview, Adobe Acrobat, DEVONthink, and presumably any other
+    application that can read the PDF metadata fields.
+
+  pdfproducer: rewrites the Producer metadata field in the PDF file. For
+    some users, this field has not utility, and thus can be usefully hijacked
+    for the purpose of storing the Zotero item URI. This makes the value
+    accessible from macOS Preview, Adobe Acrobat, DEVONthink, and presumably
+    any other application that can read the PDF metadata fields. However,
+    note that some users (archivists, forensics investigators, possibly
+    others) do use the Producer field, and overwriting it may be undesirable.
+
+Filtering by date
+~~~~~~~~~~~~~~~~~
 
 If the -d option is given, the PDF files will be filtered to use only those
 whose last-modified date/time stamp is no older than the given date/time
-description.  Valid descriptors are those accepted by the Python dateparser
-library.  Make sure to enclose descriptions within single or double
-quotes.  Examples:
+description. Valid descriptors are those accepted by the Python dateparser
+library. Make sure to enclose descriptions within single or double
+quotes. Examples:
 
-  zuppa -d "2 weeks ago" -a ....
-  zuppa -d "2014-08-29"  -a ....
-  zuppa -d "12 Dec 2014" -a ....
-  zuppa -d "July 4, 2013" -a ....
-
-To make Zuppa only print what it would do without doing it, use the -n "dry
-run" option.
-
-Methods of writing the Zotero item URI
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Zuppa supports multiple methods of writing the Zotero link.  The option -l
-will cause Zuppa to print a list of the methods available, then exit.  The
-option -m can be used to select one or more methods when running Zuppa.
-(Separate the method names with commas, without spaces.)  For example,
-
-  zuppa -m findercomment,pdfsubject ~/my-zotero/storage
-
-will make Zuppa write the Zotero select link into the Finder commands and
-the PDF metadata attribute "Subject".  At this time, the following methods
-are available:
-
-  findercomment:
-
-  pdfsubject:
-
-  pdfproducer:
-
+ zuppa -d "2 weeks ago" ....
+ zuppa -d "2014-08-29" ....
+ zuppa -d "12 Dec 2014" ....
+ zuppa -d "July 4, 2013" ....
 
 Additional command-line arguments
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+To make Zuppa only print what it would do without actually doing it, use
+the -n "dry run" option.
+
 If given the -q option, Zuppa will not print its usual informational messages
-while it is working.  It will only print messages for warnings or errors.
-By default messages printed by Zuppa are also color-coded.  If given the
-option -C, Zuppa will not color the text of messages it prints.  (This latter
+while it is working. It will only print messages for warnings or errors.
+By default messages printed by Zuppa are also color-coded. If given the
+option -C, Zuppa will not color the text of messages it prints. (This latter
 option is useful when running Zuppa within subshells inside other environments
 such as Emacs.)
 
@@ -154,7 +165,7 @@ If given the -V option, this program will print the version and other
 information, and exit without doing anything else.
 
 If given the -@ argument, this program will output a detailed trace of what it
-is doing.  The debug trace will be sent to the given destination, which can
+is doing. The debug trace will be sent to the given destination, which can
 be '-' to indicate console output, or a file path to send the output to a file.
 
 When -@ has been given, Zuppa also installs a signal handler on signal SIGUSR1
@@ -169,13 +180,13 @@ This program exits with a return code of 0 if no problems are encountered.
 It returns a nonzero value otherwise. The following table lists the possible
 return values:
 
-    0 = success -- program completed normally
-    1 = the user interrupted the program's execution
-    2 = encountered a bad or missing value for an option
-    3 = no network detected -- cannot proceed
-    4 = file error -- encountered a problem with a file
-    5 = server error -- encountered a problem with a server
-    6 = an exception or fatal error occurred
+  0 = success -- program completed normally
+  1 = the user interrupted the program's execution
+  2 = encountered a bad or missing value for an option
+  3 = no network detected -- cannot proceed
+  4 = file error -- encountered a problem with a file
+  5 = server error -- encountered a problem with a server
+  6 = an exception or fatal error occurred
 
 Command-line arguments summary
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
