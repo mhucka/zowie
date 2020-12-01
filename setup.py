c4c5a1c8f6288261c376cab9a8113b4975e1a5c7
Michael Hucka <mhucka@caltech.edu>
2020-12-01 14:08:33 -0800

    Add the usual setup.* files

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    setup.py
+# @brief   Installation setup file
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/mhucka/zowie
+#
+# Note: configuration metadata is maintained in setup.cfg.  This file exists
+# primarily to hook in setup.cfg and requirements.txt.
+#
+# =============================================================================
+
+import os
+from   os import path
+from   setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as f:
+    reqs = f.read().rstrip().splitlines()
+
+setup(
+    setup_requires = ['wheel'],
+    install_requires = reqs,
+)
