b7476581403cff5de9ed1e3985f4e9aa1b55d0de
Michael Hucka <mhucka@caltech.edu>
2020-12-08 20:43:05 -0800

    Add tests for reading & saving keyring

--- tests/test_keyring_utils.py
+++ tests/test_keyring_utils.py
@@ -10,6 +10,11 @@ except:
     sys.path.append('..')
 
 from zowie.keyring_utils import _encoded, _decoded
+from zowie.keyring_utils import keyring_credentials, save_keyring_credentials
 
 def test_encoding():
     assert _decoded(_encoded('abc', '123')) == ('abc', '123')
+
+def test_keyring():
+    save_keyring_credentials('@@testingkey@@', '@@testingid@@', '@@testring@@')
+    assert keyring_credentials('@@testring@@') == ('@@testingkey@@', '@@testingid@@')
