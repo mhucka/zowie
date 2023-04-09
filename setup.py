9b910e7f0b9e517038e8039bb1cec4018332ffa8
Michael Hucka <mhucka@caltech.edu>
2023-04-09 12:33:53 -0700

    chore: update to use versions developed for other projects

--- setup.py
+++ setup.py
@@ -8,19 +8,33 @@
 #
 # Note: configuration metadata is maintained in setup.cfg.  This file exists
 # primarily to hook in setup.cfg and requirements.txt.
-#
 # =============================================================================
 
-import os
-from   os import path
-from   setuptools import setup
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'requirements.txt')) as f:
-    reqs = f.read().rstrip().splitlines()
+def requirements(file):
+    from os import path
+    required = []
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), file)
+    if path.exists(requirements_file):
+        with open(requirements_file, encoding='utf-8') as f:
+            required = [ln for ln in filter(str.strip, f.read().splitlines())
+                        if not ln.startswith('#')]
+        if any(item.startswith(('-', '.', '/')) for item in required):
+            # The requirements.txt uses pip features. Try to use pip's parser.
+            try:
+                from pip._internal.req import parse_requirements
+                from pip._internal.network.session import PipSession
+                parsed = parse_requirements(requirements_file, PipSession())
+                required = [item.requirement for item in parsed]
+            except ImportError:
+                # No pip, or not the expected version. Give up & return as-is.
+                pass
+    return required
+
 
 setup(
-    setup_requires = ['wheel'],
-    install_requires = reqs,
+    setup_requires=['wheel'],
+    install_requires=requirements('requirements.txt'),
 )
